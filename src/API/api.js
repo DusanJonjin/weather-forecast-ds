e903f740a386cd2ae12463d8681895338f946033
Dusan Jonjin <dusan.jonjin@gmail.com>
2021-03-27 17:03:10 +0100

    Changed proxy to debug cors error

--- src/API/api.js
+++ src/API/api.js
@@ -5,9 +5,8 @@ export const getWeather = async (searchValue, badUrl, badUrlSearch, networkError
         `https://us1.locationiq.com/v1/search.php?key=pk.ae7618367e66d568a8e40af8d9c248ae&q=${searchValue}&format=json&limit=1`
         );
         const apiGeoResult = await apiGeo.json();
-        if (apiGeoResult.error) return badUrlSearch;
         const geoData = apiGeoResult[0];
-        if (!geoData) return badUrlSearch;
+        if (apiGeoResult.error || !geoData) return badUrlSearch;
         const { lat, lon, display_name } = geoData;
         if (!lat || !lon) return networkError;
         const displayNameInArr = display_name.split(',');
@@ -16,7 +15,7 @@ export const getWeather = async (searchValue, badUrl, badUrlSearch, networkError
         const place = {city, country}
     
         const apiWeather = await fetch(
-        `https://api.darksky.net/forecast/5e6a1cf83ac22f224520336d0e011527/${lat + ',' + lon}?exclude=flags,alerts,minutely&&units=ca&&extend=hourly`
+        `https://serene-basin-16003.herokuapp.com/https://api.darksky.net/forecast/5e6a1cf83ac22f224520336d0e011527/${lat + ',' + lon}?exclude=flags,alerts,minutely&&units=ca&&extend=hourly`
         );
         const apiWeatherResult = await apiWeather.json();
         const weatherWithPlace = {...apiWeatherResult, ...place}
